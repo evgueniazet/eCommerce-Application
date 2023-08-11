d054f954a053faf2b3330dcd930760ce8ae7688d
evgueniazet <zelenkoea19@gmail.com>
2023-08-11 11:58:51 +0200

    feat: add custom  hook useRegistrationForm

--- e-commerce-app/src/components/RegistrationForm/RegistrationForm.tsx
+++ e-commerce-app/src/components/RegistrationForm/RegistrationForm.tsx
@@ -1,74 +1,31 @@
 import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { useRegistrationForm } from '../../hooks/useRegistrationForm';
 
 export const RegistrationForm = () => {
-  const [email, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [birthDate, setBirthDate] = useState('');
-  const [streetAddress, setStreetAddress] = useState('');
-  const [houseNumber, setHouseNumber] = useState('');
-  const [city, setCity] = useState('');
-  const [postalCode, setPostalCode] = useState('');
-  const [country, setCountry] = useState('');
-
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
-
-  const handleRepeatPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRepeatPassword(event.target.value);
-  };
-
-  const handleFirstNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFirstName(event.target.value);
-  };
-
-  const handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLastName(event.target.value);
-  };
-
-  const handleBirthDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setBirthDate(event.target.value);
-  };
-
-  const handleStreetAddressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setStreetAddress(event.target.value);
-  };
-
-  const handleHouseNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setHouseNumber(event.target.value);
-  };
-
-  const handleCityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCity(event.target.value);
-  };
-
-  const handlePostalCodeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPostalCode(event.target.value);
-  };
-
-  const handleCountryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCountry(event.target.value);
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
+  const {
+    email,
+    password,
+    repeatPassword,
+    firstName,
+    lastName,
+    birthDate,
+    streetAddress,
+    houseNumber,
+    city,
+    postalCode,
+    country,
+    handleInputChange,
+    handleSubmit,
+  } = useRegistrationForm();
 
   return (
     <form onSubmit={handleSubmit}>
       <TextField
         label="Email"
         value={email}
-        onChange={handleEmailChange}
+        onChange={handleInputChange}
         variant="outlined"
         margin="normal"
         fullWidth
@@ -76,7 +33,7 @@ export const RegistrationForm = () => {
       <TextField
         label="Password"
         value={password}
-        onChange={handlePasswordChange}
+        onChange={handleInputChange}
         type="password"
         variant="outlined"
         margin="normal"
@@ -86,7 +43,7 @@ export const RegistrationForm = () => {
       <TextField
         label="Repeat Password"
         value={repeatPassword}
-        onChange={handleRepeatPassword}
+        onChange={handleInputChange}
         type="password"
         variant="outlined"
         margin="normal"
@@ -95,7 +52,7 @@ export const RegistrationForm = () => {
       <TextField
         label="First Name"
         value={firstName}
-        onChange={handleFirstNameChange}
+        onChange={handleInputChange}
         variant="outlined"
         margin="normal"
         fullWidth
@@ -103,7 +60,7 @@ export const RegistrationForm = () => {
       <TextField
         label="Last Name"
         value={lastName}
-        onChange={handleLastNameChange}
+        onChange={handleInputChange}
         variant="outlined"
         margin="normal"
         fullWidth
@@ -111,7 +68,7 @@ export const RegistrationForm = () => {
       <TextField
         label="Birthdate"
         value={birthDate}
-        onChange={handleBirthDateChange}
+        onChange={handleInputChange}
         variant="outlined"
         margin="normal"
         fullWidth
@@ -119,7 +76,7 @@ export const RegistrationForm = () => {
       <TextField
         label="Street Address"
         value={streetAddress}
-        onChange={handleStreetAddressChange}
+        onChange={handleInputChange}
         variant="outlined"
         margin="normal"
         fullWidth
@@ -127,7 +84,7 @@ export const RegistrationForm = () => {
       <TextField
         label="House Number"
         value={houseNumber}
-        onChange={handleHouseNumberChange}
+        onChange={handleInputChange}
         variant="outlined"
         margin="normal"
         fullWidth
@@ -135,7 +92,7 @@ export const RegistrationForm = () => {
       <TextField
         label="City"
         value={city}
-        onChange={handleCityChange}
+        onChange={handleInputChange}
         variant="outlined"
         margin="normal"
         fullWidth
@@ -143,7 +100,7 @@ export const RegistrationForm = () => {
       <TextField
         label="Postal Code"
         value={postalCode}
-        onChange={handlePostalCodeChange}
+        onChange={handleInputChange}
         variant="outlined"
         margin="normal"
         fullWidth
@@ -151,7 +108,7 @@ export const RegistrationForm = () => {
       <TextField
         label="Country"
         value={country}
-        onChange={handleCountryChange}
+        onChange={handleInputChange}
         variant="outlined"
         margin="normal"
         fullWidth
