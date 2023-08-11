f6b67b028283c9f559e52e3efa313d88e6e67aaa
evgueniazet <zelenkoea19@gmail.com>
2023-08-11 16:23:55 +0200

    feat: add interface for registration form data

---
+++ e-commerce-app/src/interfaces/IRegistrationFormData.ts
@@ -0,0 +1,13 @@
+export interface IRegistrationFormData {
+  email: string;
+  password: string;
+  repeatPassword: string;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  streetAddress: string;
+  houseNumber: string;
+  city: string;
+  postalCode: string;
+  country: string;
+}
