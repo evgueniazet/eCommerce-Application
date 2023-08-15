7a3645ed06e3fe37039f7b5c0b78d815e46f5cbf
Nargiza14 <ruzieva.aya@icloud.com>
2023-08-15 20:46:19 +0600

    init: start registration page layout

--- e-commerce-app/src/pages/RegistrationPage/RegistrationPage.tsx
+++ e-commerce-app/src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+// import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
 
 export const RegistrationPage: React.FC = () => {
-    return <div>Registration Page</div>;
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+          email: data.get('email'),
+          password: data.get('password'),
+        });
+      };
+    return (
+        <Container component="main" maxWidth="xs"></Container>
+    );
 };
