fc65aa0112c4b2fad40b09f2a15dbefabdbecea4
Nargiza14 <ruzieva.aya@icloud.com>
2023-08-19 00:11:17 +0600

    init: start error page task

--- e-commerce-app/src/pages/ErrorPage/ErrorPage.tsx
+++ e-commerce-app/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import imgError from '../../assets/images/ImgErrorPage.png';
+import { Link } from 'react-router-dom';
 export const ErrorPage: React.FC = () => {
     return <div>Error Page</div>;
 };
