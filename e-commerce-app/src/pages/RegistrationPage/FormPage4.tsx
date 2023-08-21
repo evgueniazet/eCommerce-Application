9a8d3ea7900477220ffcf6df83c934cfcfc4a1d1
Nargiza14 <ruzieva.aya@icloud.com>
2023-08-21 18:48:24 +0600

    init: start billing page

---
+++ e-commerce-app/src/pages/RegistrationPage/FormPage4.tsx
@@ -0,0 +1,11 @@
+import {
+    TextField,
+    FormControlLabel,
+    Checkbox,
+    Grid,
+    Select,
+    MenuItem,
+    InputLabel,
+  } from '@mui/material';
+  import { FC } from 'react';
+  import { IFormPageProps } from '../../interfaces/IFormPageProps';
