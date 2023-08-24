ec86ac8451ad2e0aa1bbff954b3222ca89adccfc
Nargiza14 <ruzieva.aya@icloud.com>
2023-08-25 00:54:05 +0600

    feat: add filtering and sorting functionality

--- e-commerce-app/src/pages/ProductsPage/ProductsPage.tsx
+++ e-commerce-app/src/pages/ProductsPage/ProductsPage.tsx
@@ -1,6 +1,93 @@
 import React from 'react';
 import './ProductsPage.scss';
+import { Grid, Box, Typography, FormGroup, FormControlLabel, Stack, RadioGroup, Radio } from '@mui/material';
+import { Container } from '@mui/system';
+import Checkbox from '@mui/material/Checkbox';
+import Slider from '@mui/material/Slider';
 
 export const ProductsPage: React.FC = () => {
-  return <div>Products Page</div>;
+  const [state, setState] = React.useState({
+    category1: true,
+    category2: false,
+    category3: false,
+  });
+
+  const handleChange1 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+  const { category1, category2, category3 } = state;
+
+  const [value, setValue] = React.useState<number>(30);
+
+  const handleChange2 = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number);
+  };
+
+
+  return (
+    <Grid container sx={{ height: '80vh'}}>
+      <Container component='aside'>
+        <Box>
+          <Typography variant='h4'>
+            Product Categories
+          </Typography>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Checkbox checked={category1} onChange={handleChange1} name="category1" />
+              }
+              label="Category 1"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox checked={category2} onChange={handleChange1} name="category2" />
+              }
+              label="Category 2"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox checked={category3} onChange={handleChange1} name="category3" />
+              }
+              label="Category 3"
+            />
+          </FormGroup>
+        </Box>
+
+        <Box>
+          <Typography variant='h4'>
+            Filter by price
+          </Typography>
+          <Box sx={{ width: 200 }}>
+            <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+              <p>0</p>
+              <Slider aria-label="priceRange" value={value} onChange={handleChange2} />
+              <p>1000</p>
+            </Stack>
+          </Box>
+        </Box>
+
+        <Box>
+          <Typography variant='h4'>
+            Sort by
+          </Typography>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="lowPrice"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel value="lowPrice" control={<Radio />} label="Price (lowest first)" />
+            <FormControlLabel value="highPrice" control={<Radio />} label="Price (highest first)" />
+          </RadioGroup>
+        </Box>
+      </Container>
+
+      <Container component='main'>
+        
+      </Container>
+    </Grid>
+  );
 };
