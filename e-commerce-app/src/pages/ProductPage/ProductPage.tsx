903bb5f748f7206e2402e2ac6d504e854e75bdcd
Nargiza14 <ruzieva.aya@icloud.com>
2023-08-28 01:04:36 +0600

    feat: add left side styles

--- e-commerce-app/src/pages/ProductPage/ProductPage.tsx
+++ e-commerce-app/src/pages/ProductPage/ProductPage.tsx
@@ -1,19 +1,25 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import './ProductPage.scss';
 import { Box } from '@mui/material';
+import TestImg1 from '../../assets/images/TestImg-1.png';
 import TestImg2 from '../../assets/images/TestImg-2.jpeg';
 
 export const ProductPage: FC = () => {
+  const [selectedImg, setSelectedImg] = useState(0);
+
+  const images=[
+    TestImg1, TestImg2,
+  ];
 
   return (
     <Box className='product'>
       <Box className='left'>
-        <Box className='smallImg'>
-          <img src='' alt='img1' />
-          <img src={TestImg2} alt='img2'/>
+        <Box className='images'>
+          <img src={images[0]} alt='img1' onClick={()=>setSelectedImg(0)} />
+          <img src={images[1]} alt='img2' onClick={()=>setSelectedImg(1)}/>
         </Box>
         <Box className='bigImg'>
-          <img src='' alt=''/>
+        <img src={images[selectedImg]} alt='' />
         </Box>
       </Box>
 
