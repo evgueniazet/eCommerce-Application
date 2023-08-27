1788df0961eb514bdc8fb39171ec68de97b83ab0
Nargiza14 <ruzieva.aya@icloud.com>
2023-08-27 23:41:45 +0600

    feat: add second test image

--- e-commerce-app/src/pages/ProductPage/ProductPage.tsx
+++ e-commerce-app/src/pages/ProductPage/ProductPage.tsx
@@ -1,6 +1,23 @@
-import {FC} from 'react';
+import { FC } from 'react';
 import './ProductPage.scss';
+import { Box } from '@mui/material';
+import TestImg2 from '../../assets/images/TestImg-2.jpeg';
 
 export const ProductPage: FC = () => {
-  return <div>Product Page</div>;
+
+  return (
+    <Box className='product'>
+      <Box className='left'>
+        <Box className='smallImg'>
+          <img src='' alt='img1' onClick={e=>setSelectedImg(0)}/>
+          <img src={TestImg2} alt='img2' onClick={e=>setSelectedImg(1)}/>
+        </Box>
+        <Box className='bigImg'>
+          <img src={smallImg[selectedImg]} alt=''/>
+        </Box>
+      </Box>
+
+      <Box className='right'></Box>
+    </Box>
+  );
 };
