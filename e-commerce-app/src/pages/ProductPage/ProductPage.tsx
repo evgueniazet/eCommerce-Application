d6309f716ef2284ce714290cd61ef1edc060f033
Nargiza14 <ruzieva.aya@icloud.com>
2023-08-30 01:16:56 +0600

    fix: change modal top position

--- e-commerce-app/src/pages/ProductPage/ProductPage.tsx
+++ e-commerce-app/src/pages/ProductPage/ProductPage.tsx
@@ -14,7 +14,7 @@ import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
 
 const style = {
   position: 'absolute',
-  top: '40%',
+  top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: '36%',
