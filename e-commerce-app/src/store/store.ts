cc132b805698c9a5499da98ed43919102b13355b
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-08-30 16:54:46 +0300

    feat: add products middleware to the store

--- e-commerce-app/src/store/store.ts
+++ e-commerce-app/src/store/store.ts
@@ -2,15 +2,21 @@ import { configureStore } from '@reduxjs/toolkit';
 import { authApi } from '../api/authApi';
 import { UserReducer } from './slices/userSlice';
 import { myCustomerApi } from '../api/myCustomerApi';
+import { productsApi } from '../api/productsApi';
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
     [myCustomerApi.reducerPath]: myCustomerApi.reducer,
+    [productsApi.reducerPath]: productsApi.reducer,
     user: UserReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({}).concat([authApi.middleware, myCustomerApi.middleware]),
+    getDefaultMiddleware({}).concat([
+      authApi.middleware,
+      myCustomerApi.middleware,
+      productsApi.middleware,
+    ]),
 });
 
 export type RootStateType = ReturnType<typeof store.getState>;
