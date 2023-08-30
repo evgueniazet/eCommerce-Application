4690d3c64353615fcb79839e10572a07a28942b6
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-08-31 01:05:22 +0300

    feat: add LoadingProgress on main page

--- e-commerce-app/src/components/ProductCard/ProductCard.tsx
+++ e-commerce-app/src/components/ProductCard/ProductCard.tsx
@@ -19,40 +19,39 @@ export const ProductCard: FC<ICardProps> = ({ item }) => {
 
   const currencyCommon = item.masterData.current.masterVariant.prices[0].value.currencyCode;
   const priceCommon = new Intl.NumberFormat('en-IN', {
-      style: 'currency',
-      currency: currencyCommon
-    }).format(item.masterData.current.masterVariant.prices[0].value.centAmount /
-      (10 * item.masterData.current.masterVariant.prices[0].value.fractionDigits))
-  ;
-
-
+    style: 'currency',
+    currency: currencyCommon,
+  }).format(
+    item.masterData.current.masterVariant.prices[0].value.centAmount /
+      (10 * item.masterData.current.masterVariant.prices[0].value.fractionDigits),
+  );
   const currencySale = item.masterData.current.masterVariant.prices[1].value.currencyCode;
   const priceSale = new Intl.NumberFormat('en-IN', {
     style: 'currency',
-    currency: currencySale
-  }).format(item.masterData.current.masterVariant.prices[1].value.centAmount /
-    (10 * item.masterData.current.masterVariant.prices[1].value.fractionDigits));
-
+    currency: currencySale,
+  }).format(
+    item.masterData.current.masterVariant.prices[1].value.centAmount /
+      (10 * item.masterData.current.masterVariant.prices[1].value.fractionDigits),
+  );
 
   return (
     <Card className={styles.card}>
       <Box width={'100%'}>
-        <CardMedia sx={{ height: 200, width: '100%' }}
-                   image={imgPath}
-                   title={imgDescription}/>
+        <CardMedia sx={{ height: 200, width: '100%' }} image={imgPath} title={imgDescription} />
       </Box>
       <CardContent className={styles.card__text}>
-        <Typography className={styles.card__title}
-                    component="h2">{item.masterData.current.name.en}</Typography>
-        <Typography className={styles.card__price}
-                    component="h3">{priceCommon}</Typography>
-        <Typography className={styles.card__price}
-                    component="h3">{priceSale}</Typography>
+        <Typography className={styles.card__title} component="h2">
+          {item.masterData.current.name.en}
+        </Typography>
+        <Typography className={styles.card__price} component="h3">
+          {priceCommon}
+        </Typography>
+        <Typography className={styles.card__price} component="h3">
+          {priceSale}
+        </Typography>
       </CardContent>
       <CardActions>
-        <Button onClick={handlerNavigation}
-                color="success"
-                variant="outlined">
+        <Button onClick={handlerNavigation} color="success" variant="outlined">
           Read more
         </Button>
       </CardActions>
