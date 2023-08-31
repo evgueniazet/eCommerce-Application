b47a70ad0105b2a46a2d7c833cacc867f5c1293d
Nargiza14 <ruzieva.aya@icloud.com>
2023-09-01 00:29:42 +0600

    feat: add navigation on the page

--- e-commerce-app/src/pages/UserPage/UserPage.tsx
+++ e-commerce-app/src/pages/UserPage/UserPage.tsx
@@ -1,6 +1,99 @@
-import { FC } from 'react';
+import React, { useState } from 'react';
 import './UserPage.module.scss';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-export const UserPage: FC = () => {
-  return <div>User Page</div>;
+const steps = ['Personal information', 'Shipping/Billing address', 'Change password'];
+
+export const UserPage: React.FC = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  const [skipped, setSkipped] = useState(new Set<number>());
+
+  const isStepSkipped = (step: number) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box sx={{ width: '100%', mt: '20%' }}>
+        <Stepper activeStep={activeStep} alternativeLabel>
+          {steps.map((label, index) => {
+            const stepProps: { completed?: boolean } = {};
+
+            if (isStepSkipped(index)) {
+              stepProps.completed = false;
+            }
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === steps.length ? (
+          <Box>
+            <Typography sx={{ mt: 2, mb: 1 }}>All pages completed - you&apos;re updated</Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleReset}>Reset</Button>
+            </Box>
+          </Box>
+        ) : (
+          <Box>
+            <Typography sx={{ mt: 2, mb: 1 }}>Page {activeStep + 1}</Typography>
+            <Box
+              sx={{
+                marginTop: 8,
+                component: 'form',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            ></Box>
+            <Box>Nargiza</Box>
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Back
+              </Button>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleNext}>
+                {activeStep === steps.length - 1 ? 'Done' : 'Next'}
+              </Button>
+            </Box>
+          </Box>
+        )}
+      </Box>
+    </Container>
+  );
 };
