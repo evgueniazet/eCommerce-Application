f4660c5ce48d99bb515e6d992fccc349b107b39f
evgueniazet <zelenkoea19@gmail.com>
2023-09-01 15:06:22 +0200

    feat: add message for empty product list

---
+++ e-commerce-app/src/components/ProductsList/EmptyProducts.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import styles from './ProductsList.module.scss';
+import { Box } from '@mui/material';
+import empty from '../../assets/images/EmptyCatalogPage.svg';
+
+export const EmptyProducts: FC = () => {
+  return (
+    <Box className={styles.empty__products}>
+      This is an empty product list. There are no products available!
+      <img src={empty} alt="home-icon" className={styles.img}/>
+    </Box>
+  );
+};
