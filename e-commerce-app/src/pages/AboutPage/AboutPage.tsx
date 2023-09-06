194f4528abee9d5903d8c329953928f2addbcdab
Nargiza14 <ruzieva.aya@icloud.com>
2023-09-07 00:12:29 +0600

    feat: add cards info

--- e-commerce-app/src/pages/AboutPage/AboutPage.tsx
+++ e-commerce-app/src/pages/AboutPage/AboutPage.tsx
@@ -7,10 +7,12 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import Avatar from '@mui/material/Avatar';
 import Link from '@mui/material/Link';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import Ilia from '../../assets/AboutPageImg/Ilia.jpeg';
+import Divider from '@mui/material/Divider';
+import Ilya from '../../assets/AboutPageImg/Ilya.jpeg';
 import Evguenia from '../../assets/AboutPageImg/Evguenia.jpeg';
 import Nargiza from '../../assets/AboutPageImg/Nargiza.jpg';
 
@@ -26,8 +28,8 @@ export const AboutPage: React.FC = () => {
       >
         <Container maxWidth="sm">
           <Typography
-            component="h4"
-            variant="h2"
+            component="h5"
+            variant="h3"
             align="center"
             color="green"
             fontWeight="500"
@@ -40,7 +42,7 @@ export const AboutPage: React.FC = () => {
           </Typography>
           <Stack sx={{ pt: 4 }} direction="row" spacing={2} justifyContent="center">
             <Button variant="contained">Join RS School</Button>
-            <Button variant="outlined">Our mentor</Button>
+            <Button variant="outlined">Team&apos;s mentor</Button>
           </Stack>
         </Container>
       </Box>
@@ -48,34 +50,70 @@ export const AboutPage: React.FC = () => {
         <Grid container spacing={4}>
           <Grid item xs={12} sm={6} md={4}>
             <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-              <Box display="flex" justifyContent="space-between">
-                <Avatar alt="Elijah" src={Ilia} sx={{ width: 56, height: 56 }} />
+              <Box display="flex" justifyContent="space-between" alignItems="center" padding="20px">
+                <Avatar alt="Ilya" src={Ilya} sx={{ width: 56, height: 56 }} />
                 <Link href="https://github.com/LikeKugi" target="_blank">
                   <GitHubIcon color="disabled" fontSize="large" />
                 </Link>
               </Box>
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h6" component="h2">
+                  Ilya Shcherbakov
+                </Typography>
+                <Typography fontSize={14} mb={3}>
+                  Front-end developer
+                </Typography>
+                <Divider variant="middle" />
+                <Typography color="grey" fontSize={12} mt={1.5}>
+                  Python developer, meme creator, great sense of humor.
+                </Typography>
+              </CardContent>
             </Card>
           </Grid>
 
           <Grid item xs={12} sm={6} md={4}>
             <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-              <Box display="flex">
+              <Box display="flex" justifyContent="space-between" alignItems="center" padding="20px">
                 <Avatar alt="Evguenia" src={Evguenia} sx={{ width: 56, height: 56 }} />
                 <Link href="https://github.com/evgueniazet" target="_blank">
                   <GitHubIcon color="disabled" fontSize="large" />
                 </Link>
               </Box>
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h6" component="h2">
+                  Evguenia Zelenko
+                </Typography>
+                <Typography fontSize={14} mb={3}>
+                  Front-end developer
+                </Typography>
+                <Divider variant="middle" />
+                <Typography color="grey" fontSize={12} mt={1.5}>
+                  Spotify lover, strong motivator, always positive.
+                </Typography>
+              </CardContent>
             </Card>
           </Grid>
 
           <Grid item xs={12} sm={6} md={4}>
             <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-              <Box display="flex">
+              <Box display="flex" justifyContent="space-between" alignItems="center" padding="20px">
                 <Avatar alt="Nargiza" src={Nargiza} sx={{ width: 56, height: 56 }} />
                 <Link href="https://github.com/Nargiza14" target="_blank">
                   <GitHubIcon color="disabled" fontSize="large" />
                 </Link>
               </Box>
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h6" component="h2">
+                  Nargiza Ruzieva
+                </Typography>
+                <Typography fontSize={14} mb={3}>
+                  Front-end developer
+                </Typography>
+                <Divider variant="middle" />
+                <Typography color="grey" fontSize={12} mt={1.5}>
+                  Mother-of-three, karaoke lover, quet person.
+                </Typography>
+              </CardContent>
             </Card>
           </Grid>
         </Grid>
