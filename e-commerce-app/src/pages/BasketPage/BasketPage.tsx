18e331c16a0cd8fd3eb537924a3dc715d4998db4
Nargiza14 <ruzieva.aya@icloud.com>
2023-09-08 23:34:58 +0600

    init: start basket page layout

--- e-commerce-app/src/pages/BasketPage/BasketPage.tsx
+++ e-commerce-app/src/pages/BasketPage/BasketPage.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
-export const BasketPage: React.FC = () => {
-  return <div>Basket Page</div>;
+export const BasketPage: FC = () => {
+  const cart = useSelector((state) => state.cart);
+  return (
+    <Container>
+      <Typography variant="h2">Shopping cart</Typography>
+    </Container>
+  );
 };
