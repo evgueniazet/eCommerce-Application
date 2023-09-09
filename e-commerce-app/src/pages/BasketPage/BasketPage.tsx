210dd025eb846d6a8dacc7c42975ab20853e64c7
Nargiza14 <ruzieva.aya@icloud.com>
2023-09-09 22:46:06 +0600

    feat: add empty cart page layout

--- e-commerce-app/src/pages/BasketPage/BasketPage.tsx
+++ e-commerce-app/src/pages/BasketPage/BasketPage.tsx
@@ -1,13 +1,35 @@
 import { FC } from 'react';
-import { useSelector } from 'react-redux';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import { Box } from '@mui/system';
+import { useNavigate } from 'react-router-dom';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import { Button } from '@mui/material';
+// import Grid from '@mui/material/Grid';
+import EmptyBasket from '../../assets/images/BasketPageImg.png';
 
 export const BasketPage: FC = () => {
-  const cart = useSelector((state) => state.cart);
+  const navigate = useNavigate();
   return (
-    <Container>
-      <Typography variant="h2">Shopping cart</Typography>
+    <Container className="container" sx={{ textAlign: 'center', mt: '5%' }}>
+      <Typography variant="h3" fontWeight="500" color="green">
+        Shopping cart
+      </Typography>
+      <Box className="cart-empty">
+        <Typography variant="h5" mt="5%">
+          Your cart is empty
+        </Typography>
+        <Button
+          className="start-shopping"
+          sx={{ backgroundColor: 'yellowgreen', color: 'green', mt: '5%' }}
+        >
+          <KeyboardBackspaceIcon width="20" />
+          <Box component="span" onClick={() => navigate('/products')}>
+            Start shopping
+          </Box>
+        </Button>
+      </Box>
+      <img src={EmptyBasket} alt="EmptyBasket" width={300} />
     </Container>
   );
 };
