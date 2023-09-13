b4026529fac22b54646fbc3dd117ac3aa7853c0f
Nargiza14 <ruzieva.aya@icloud.com>
2023-09-13 23:25:01 +0600

    init: start home page layout

--- e-commerce-app/src/pages/HomePage/HomePage.tsx
+++ e-commerce-app/src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useAppSelector } from '../../store/hooks';
+import Container from '@mui/material/Container';
+import styles from './HomePage.module.scss';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 export const HomePage: React.FC = () => {
   const { email } = useAppSelector((state) => state.user);
   return (
-    <div>
-      Home Page <p>Hello, {email ? email : 'Stranger. Can I call you a Friend?'}</p>
-    </div>
+    <Container component="div" className={styles.homePage}>
+      <Box className={styles.banner}>
+        <Typography component="p" paragraph>
+          Hello, {email ? email : 'Stranger. Can I call you a Friend?'}
+        </Typography>
+      </Box>
+    </Container>
   );
 };
