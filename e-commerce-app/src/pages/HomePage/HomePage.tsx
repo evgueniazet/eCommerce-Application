d5cf22c59d477c4ce976762f4f4cd261b910c91a
Nargiza14 <ruzieva.aya@icloud.com>
2023-09-14 13:54:35 +0600

    feat: add info section

--- e-commerce-app/src/pages/HomePage/HomePage.tsx
+++ e-commerce-app/src/pages/HomePage/HomePage.tsx
@@ -27,7 +27,7 @@ export const HomePage: React.FC = () => {
         </Typography>
         <Grid container spacing={4} textAlign="center">
           <Grid item xs={12} sm={6} md={4}>
-            <Card sx={{ minWidth: 275 }}>
+            <Card>
               <CardContent>
                 <Avatar
                   alt="Promo1"
@@ -50,7 +50,7 @@ export const HomePage: React.FC = () => {
             </Card>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-            <Card sx={{ minWidth: 275 }}>
+            <Card>
               <CardContent>
                 <Avatar
                   alt="Promo2"
@@ -73,7 +73,7 @@ export const HomePage: React.FC = () => {
             </Card>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-            <Card sx={{ minWidth: 275 }}>
+            <Card>
               <CardContent>
                 <Avatar
                   alt="Promo3"
@@ -97,6 +97,25 @@ export const HomePage: React.FC = () => {
           </Grid>
         </Grid>
       </Container>
+      <Grid container className={styles.infoContainer} spacing={2}>
+        <Grid item className={styles.infoImage} xs={12} sm={7} md={6}></Grid>
+        <Grid item xs={12} sm={7} md={6}>
+          <Box>
+            <Typography className={styles.info_title}>We offer Plants Delivery</Typography>
+            <Typography component="p" className={styles.info_text}>
+              Make celebrations particularly special by sending a friend or family member one of our
+              select plants with a personalized message! You can never go wrong with sending one of
+              our best sellers to show just how much you care. They can mark the occasion and serve
+              as a lasting reminder of the event. Certain plants may also have cultural or symbolic
+              significance, making them even more meaningful as gifts for specific celebrations.
+              Unlike cut flowers that wither within days, plants are long-lasting gifts that can
+              continue to grow and thrive for months or even years with proper care. Make sure to
+              point them in the direction of our catalog page that cover almost all of the plants we
+              have in stock.
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
