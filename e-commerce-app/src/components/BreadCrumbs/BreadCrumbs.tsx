b573ce794ac283324cec2c4f250342b602c65232
Nargiza14 <ruzieva.aya@icloud.com>
2023-09-16 01:04:40 +0600

    feat: add breadcrumb structure

--- e-commerce-app/src/components/BreadCrumbs/BreadCrumbs.tsx
+++ e-commerce-app/src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import styles from './BreadCrumbs.module.scss';
-import { Link, useLocation } from 'react-router-dom';
-import Box from '@mui/material/Box';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+
+function handleClick(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
+}
 
 export const BreadCrumbs: React.FC = () => {
-  const location = useLocation();
-  const matchRoutes = location.pathname
-    .split('/')
-    .filter(Boolean)
-    .map((route) => decodeURIComponent(route));
+  const breadcrumbs = [
+    <Link underline="hover" key="1" color="inherit" href="/" onClick={handleClick}>
+      Home
+    </Link>,
+    <Link
+      underline="hover"
+      key="2"
+      color="inherit"
+      href="/material-ui/getting-started/installation/"
+      onClick={handleClick}
+    >
+      Products
+    </Link>,
+    <Typography key="3" color="text.primary">
+      Categories
+    </Typography>,
+  ];
 
-  return <Box className={'styles.breadcrumbs - container'}></Box>;
+  return (
+    <Stack spacing={2}>
+      <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+        {breadcrumbs}
+      </Breadcrumbs>
+    </Stack>
+  );
 };
