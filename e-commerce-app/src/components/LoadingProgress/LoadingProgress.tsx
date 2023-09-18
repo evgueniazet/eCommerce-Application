9f55b9909fdbdfb5bb0e547a1c8543ba868e679c
evgueniazet <zelenkoea19@gmail.com>
2023-09-18 10:46:14 +0300

    fix: change Loading Progress styles

--- e-commerce-app/src/components/LoadingProgress/LoadingProgress.tsx
+++ e-commerce-app/src/components/LoadingProgress/LoadingProgress.tsx
@@ -7,6 +7,8 @@ const LoadingProgress = (): JSX.Element => {
   return (
     <Box display="flex" justifyContent="center" alignItems="center" className={styles.box}>
       <CircularProgress
+        size={200}
+        thickness={10}
         style={{ width: 200, height: 200, strokeWidth: 10 }}
         data-testid="circular-progress"
       />
